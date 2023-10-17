90e9406d86de203449ab09166b9c91b141d8a2e2
Snehal Senapati <78613699+snehalsenapati23@users.noreply.github.com>
2023-10-17 23:25:26 +0530

    Add files via upload

---
+++ src/configs/sidebarNav.js
@@ -0,0 +1,41 @@
+const sidebarNav = [
+    {
+        link: '/',
+        section: 'home',
+        icon: <i className='bx bx-home-alt'></i>,
+        text: 'Dashboard'
+    },
+    {
+        link: '/products',
+        section: 'products',
+        icon: <i className='bx bx-cube'></i>,
+        text: 'Products'
+    },
+    {
+        link: '/customers',
+        section: 'customers',
+        icon: <i className='bx bx-user'></i>,
+        text: 'Customers'
+    },
+    {
+        link: '/income',
+        section: 'income',
+        icon: <i className='bx bx-receipt' ></i>,
+        text: 'Income'
+    },
+  
+    {
+        link: '/promote',
+        section: 'stats',
+        icon: <i className='bx bx-line-chart'></i>,
+        text: 'Promote'
+    },
+    {
+        link: '/help',
+        section: 'settings',
+        icon: <i className='bx bx-cog'></i>,
+        text: 'Help'
+    }
+]
+
+export default sidebarNav
